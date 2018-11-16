508d066f7a5a495a2456a71709e9000b7b63058e
nicejames <leessju@gmail.com>
2018-11-16 15:12:11 +0900

    npm i redux-devtools-extension --save

--- client/src/store.js
+++ client/src/store.js
@@ -1,26 +1,34 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import roootReducer from './reducers';
+
 
 const initialState = {};
 const middleware = [thunk];
 
-let store;
+const store = createStore(
+  roootReducer, 
+  initialState, 
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
-if (window.navigator.userAgent.includes('Chrome')) {
-  store = createStore(
-    roootReducer, 
-    initialState, 
-    compose(
-      applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    ) 
-  );
-} else {
-  store = createStore(
-    roootReducer, 
-    initialState, 
-    applyMiddleware(...middleware));
-}
+// const store = createStore(
+//   roootReducer, 
+//   initialState, 
+//   compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   ) 
+// );
+
+// if (window.navigator.userAgent.includes('Chrome')) {
+  
+// } else {
+//   store = createStore(
+//     roootReducer, 
+//     initialState, 
+//     applyMiddleware(...middleware));
+// }
 
 export default store;
